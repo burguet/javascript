89eec708446db04da2256308cc63916ac04ff73f
burguet <lucasburguet22020@gmail.com>
2023-03-03 16:49:38 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,40 @@
+//var toto = "titi";
+//alert (toto);
+
+//var MavariableString ="toto";
+//var MavariableNumerique = 100;
+//var Mavariabletableau = ['pomme', 'banana'];
+//var Mavariableobjet = window;
+
+//alert (MavariableNumerique + MavariableString + Mavariableobjet.location.pathname + Mavariabletableau[0]);
+
+
+//var Mavariablestring2 = "toto";
+
+//var MavariableNumerique2 = prompt("entrez un nombre", 100);
+
+//if (Mavariablestring2 == "toto" && MavariableNumerique2 !=200 || MavariableNumerique2 <=100 )
+//{
+  //  alert("je suis le cas 1");
+//}else{
+  //  alert("je suis le cas 2");
+//}
+
+//var largeur = innerWidth;
+
+//if (largeur<=300)
+//{
+  //  alert("votre pasge est trop petite");
+//}
+
+var nombre = prompt("entrez un nombre", 100);
+var resultat=1;
+
+for (var i=1; i<=nombre; i++)
+{
+    
+    resultat= i*resultat;
+}
+
+alert("resultat :" + resultat);
+
---
+++ test.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+<script src='main.js'></script>
+</head>
+<body>
+    <h1> Bienvenue en JS : <?= "coucou"?> </h1>
+
+    <script langage="javascript">
+        //alert("coucou je suis java");
+        //alert("script");
+    </script>
+</body>
+</html>
