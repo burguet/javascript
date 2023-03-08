d549edabdf416b60648c671d2938f1fbacdc5982
burguet <lucasburguet22020@gmail.com>
2023-03-08 17:43:33 +0100

    exo10

--- main.js
+++ main.js
@@ -37,37 +37,101 @@
 
 //alert("resultat :" + resultat);
 
-function ModifText()
-{
-  alert ("je vais changer la div");
+//function ModifText()
+//{
+  //alert ("je vais changer la div");
 
-  var Madiv = document.getElementsByName("test");
+  //var Madiv = document.getElementsByName("test");
 
-  Madiv.forEach(function(element) 
-  {
+  //Madiv.forEach(function(element) 
+  //{
     
-    element.innerHTML = "Hello";
+   // element.innerHTML = "Hello";
 
-  });
+  //});
 
-}
-
-
-function ModifText2() {
-  alert(" modifier la div !");
-  var Madiv = document.getElementsByTagName("div");
-
-  for (var i = 0; i < Madiv.length; i++) {
-    Madiv[i].innerHTML = "Hello2";
-  }
-}
+//}
 
 
-function ModifText3() {
-  alert("Je vais modifier la div !");
-  var Madiv = document.getElementsByClassName("test");
+//function ModifText2() {
+ // alert(" modifier la div !");
+  //var Madiv = document.getElementsByTagName("div");
+
+ // for (var i = 0; i < Madiv.length; i++) {
+   // Madiv[i].innerHTML = "Hello2";
+ // }
+//}
+
+
+//function ModifText3() {
+  //alert("Je vais modifier la div !");
+  //var Madiv = document.getElementsByClassName("test");
   
-  for (var i = 0; i < Madiv.length; i++) {
-    Madiv[i].innerHTML = "Hello3";
-  }
+  //for (var i = 0; i < Madiv.length; i++) {
+  //  Madiv[i].innerHTML = "Hello3";
+ // }
+//}
+
+var Madivtest = document.getElementById("test");
+
+Madiv.addEventListener("click", changement);
+
+function changement(evenement)
+{
+  evenement.target.innerHTML ="on m'a cliqué dessus";
+}
+
+
+
+var compteur = 0;
+var compteur2 = 0;
+
+var MaDivtest2 = document.getElementById("Methode1");
+MaDiv2.addEventListener("click", clic);
+
+var MaDivtest2 = document.getElementById("Methode2");
+MaDiv2.addEventListener("mouseover", survole);
+
+var MaDivtest2 = document.getElementById("Methode3");
+MaDiv2.addEventListener("keydown", ecriture);
+
+
+
+function ecriture(evenement) {
+    evenement.target.innerHTML = "On a écrit : "+evenement.key+" fois !";
+}
+
+function clic(evenement) {
+    compteur++;
+    evenement.target.innerHTML = "On m'a cliqué dessus "+compteur+" fois !";
+}
+
+function survole(evenement) {
+    compteur2++;
+    evenement.target.innerHTML = "On m'a survolé "+compteur2+" fois !";
+}
+
+
+
+// Exercices
+
+
+var MaDiv4 = document.getElementById("Methode4");
+MaDiv4.addEventListener("click", changementDiv);
+
+var MaDiv5 = document.getElementById("Methode5");
+MaDiv5.addEventListener("mouseover", visibilite);
+
+
+function changementDiv(evenement) {
+    if (evenement.target.className === "testclass1") {
+        evenement.target.className = "testclass2";
+    } else {
+        evenement.target.className = "testclass1";
+    }
+}
+
+function visibilite(evenement) {
+    MaDiv5.style.backgroundColor = 'rgba(255, 255, 255, .4)';
+    MaDiv5.innerHTML = '';
 }
--- test.php
+++ test.php
@@ -4,10 +4,11 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" media="screen" href="test.css">
     <title>Document</title>
 </head>
 <body>
-<style>.bouton{display:inline-block;padding:10px 20px;font-size:16px;font-weight:700;text-align:center;text-decoration:none;color:#fff;background-color:#4caf50;border-radius:5px;border:none;cursor:pointer}</style>
+<!--<style>.bouton{display:inline-block;padding:10px 20px;font-size:16px;font-weight:700;text-align:center;text-decoration:none;color:#fff;background-color:#4caf50;border-radius:5px;border:none;cursor:pointer}</style>
    
     <div class="test" name="test">coucou</div>
     <div class="test" name="test">coucou</div>
@@ -16,6 +17,40 @@
     <div class="bouton" onclick="ModifText();" >GetElementByName</div><br><br>
     <div class="bouton" onclick="ModifText2();">GetElementByTagName</div><br><br>
     <div class="bouton" onclick="ModifText3();">GetElementByClassName</div>
+-->
+
+<h1 style="text-align: center;color:orange">Exercice 10 sur le JavaScript</h1>
+    <h3 style="text-align: center;color:orange">EventListener</h3>
+    <div class="divA">
+        <div id="test" class="testclass">
+            coucou
+        </div>
+        <div id="Methode1" class="testclass">
+            Clique moi dessus !
+        </div>
+        <div id="Methode2" class="testclass">
+            Survole moi !
+        </div>
+        <div id="Methode3" class="testclass" tabindex="0">
+            Appuie au clavier
+        </div>
+        </div>
+    
+    <h3 style="text-align: center;color:orange">Première question</h3>
+
+    <div class="divA">
+        <div id="Methode4" class="testclass1">
+            Cette div changera de couleur si tu appuies dessus !
+        </div>
+    </div>
+
+    <h3 style="text-align: center;color:orange">Deuxième question</h3>
+    <div class="divA">
+        <div id="Methode5" class="testclass">
+            
+            Clique pour me faire disparaitre
+        </div>
+     </div>
 </body>
 <script src="main.js"></script>
 
